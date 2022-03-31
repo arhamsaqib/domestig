8d6def2e9fd492d85a8724f827d687727b80708e
Muhammad Arham Saqib <arhamsaqib@outlook.com>
2022-03-31 05:00:02 +0500

    changed URL and fixed http calls

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,8 +1,8 @@
-export const URL = 'http://127.0.0.1:8000/api/'; //IOS
-export const MEDIA_URL = 'http://127.0.0.1:8000/'; //IOS
+// export const URL = 'http://127.0.0.1:8000/api/'; //IOS
+// export const MEDIA_URL = 'http://127.0.0.1:8000/'; //IOS
 //export const URL = "http://10.0.2.2:8000/api/"; //Android
 
 export const GOOGLE_MAPS_API_KEY = 'AIzaSyDlMojnSF1RqIUXwYb0IyHIZXXGodvLPQQ';
 
-// export const URL = 'https://herosolutions.com.pk/arham/domestig/public/api/'; //REMOTE
-// export const MEDIA_URL = 'https://herosolutions.com.pk/arham/domestig/public/'; //REMOTE
+export const URL = 'http://159.65.31.207/api/'; //REMOTE
+export const MEDIA_URL = 'http://159.65.31.207/'; //REMOTE
